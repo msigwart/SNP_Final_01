e770bb6210a54d19bbc04b101f40cd88e810384c
Marten Sigwart <marten.sigwart@haw-hamburg.de>
2015-01-13 22:02:02 +0100

    Commit

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,5 +1,6 @@
 package simulation;
 
+
 public class Simulation {
 
 	/**
@@ -8,6 +9,15 @@ public class Simulation {
 	public static void main(String[] args) {
 		System.out.printf("Hello Simulator 123\n");
 
+		long t1, t2;
+		t1 = Time.getTimeStamp();
+		System.out.printf( "the time is: %s\n", Time.getTimeStampString());
+		t2 = Time.getTimeStamp();
+		System.out.printf( "MilliSeconds# %d\n", t2-t1 );
+		
+		SendConnection sc = new SendConnection();
+		sc.start();
+		
 	}//main
 
 }//Simulation
