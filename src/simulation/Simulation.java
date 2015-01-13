a59844a4b51b0e3cf3af36545b447d27bdb50f3d
Marten Sigwart <marten.sigwart@haw-hamburg.de>
2015-01-13 19:31:55 +0100

    Created package simulation with (main) Class Simulation

---
+++ src/simulation/Simulation.java
@@ -0,0 +1,12 @@
+package simulation;
+
+public class Simulation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.printf("Hello Simulator\n");
+	}//main
+
+}//Simulation
