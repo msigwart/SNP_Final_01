b5d59762485d14ff72daf328a3ffd575636cc5df
JOKAZ12345 <jokaz12345@gmail.com>
2015-01-13 21:04:35 +0000

    this is a test

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -6,7 +6,7 @@ public class Simulation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.printf("Hello Simulator\n");
+		System.out.printf("Hello Simulatorr\n");
 	}//main
 
 }//Simulation
