83fba5154c95c6cc358c78876cfb56c8ffc63212
Bernardo Paulino <bernardo.paulino4@gmail.com>
2015-01-14 20:30:17 +0100

    method writeEventIntoFile is now working

--- src/statistics/Statistics.java
+++ src/statistics/Statistics.java
@@ -37,11 +37,21 @@ public class Statistics {
             //Whatever the file path is.
 			System.out.println("trying to write in the file statsTest.txt");
             File statText = new File(outputFile);
-            FileOutputStream is = new FileOutputStream(statText);
-            OutputStreamWriter osw = new OutputStreamWriter(is);    
-            Writer w = new BufferedWriter(osw);
-            w.append(event.toString());
-            w.close();
+            //FileOutputStream is = new FileOutputStream(statText);
+            //OutputStreamWriter osw = new OutputStreamWriter(is);    
+            //Writer w = new BufferedWriter(osw);
+            //w.append(event.toString());
+            //w.close();
+            
+            
+          //true = append file
+    		FileWriter fileWritter = new FileWriter(statText.getName(),true);
+	        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+	        bufferWritter.write(event.toString() + "\n");
+	        bufferWritter.close();
+            
+            
+            
         } catch (IOException e) {
             System.err.println("Problem writing to the file statsTest.txt");
         }//catch
