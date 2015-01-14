ee2d03ecbe2418fe6751f48a99c2c16e95b158b4
Bernardo Paulino <bernardo.paulino4@gmail.com>
2015-01-14 03:38:25 +0100

    Class Statistic methods changed

--- src/simulation/Statistic.java
+++ src/simulation/Statistic.java
@@ -13,10 +13,10 @@ public class Statistic {
 	private ArrayList<Event> events = new ArrayList<Event>();
 	
 	public void createEvent(int eventType, Packet packet){
-		//writeEventIntoFile(new Event(eventType, packet));
+		writeEventIntoFile(new Event(eventType, packet));
 	}
 	
-	public void writeEventIntoFile(/*Event event*/){
+	public void writeEventIntoFile(Event event){
 		try {
             //Whatever the file path is.
 			System.out.println("trying to write in the file statsTest.txt");
@@ -24,7 +24,7 @@ public class Statistic {
             FileOutputStream is = new FileOutputStream(statText);
             OutputStreamWriter osw = new OutputStreamWriter(is);    
             Writer w = new BufferedWriter(osw);
-            w.write("POTATO!!!");
+            w.write(event.toString());
             w.close();
         } catch (IOException e) {
             System.err.println("Problem writing to the file statsTest.txt");
