bacf20289e498e5608345b0a0b7abb2cf1b251cb
Bernardo Paulino <bernardo.paulino4@gmail.com>
2015-01-18 19:24:44 +0100

    Class SimulationEvironment changed

--- src/simulation/SimulationEnvironment.java
+++ src/simulation/SimulationEnvironment.java
@@ -1,10 +1,33 @@
 package simulation;
 
-public class SimulationEnvironment {
+import java.util.Scanner;
 
+public class SimulationEnvironment {
+	
 	public static void main(String[] args) {
-		Simulator simulator = new Simulator(4, 1);
-		simulator.runSimulation();
+		int runs;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many simulations do you want to do?:");
+		
+		while(true){
+			while(!(sc.hasNextInt())){
+				sc.next();
+			}//while
+			
+			runs = sc.nextInt();
+			if(runs > 0){
+				System.out.print("Runs = " + runs);
+				break;
+			}
+		}//while
+		
+		System.out.print("Runs = " + runs);
+		
+		//Simulator simulator = new Simulator(4, 1);
+		//simulator.runSimulation();
+		
+		
 	}
 
 }
