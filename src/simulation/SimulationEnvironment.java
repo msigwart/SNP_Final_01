07c9c31447462f5b7a087106bb5fb8eeccc8b83b
Bernardo Paulino <bernardo.paulino4@gmail.com>
2015-01-18 20:45:09 +0100

    The simulation environment can run now multiple simulations

--- src/simulation/SimulationEnvironment.java
+++ src/simulation/SimulationEnvironment.java
@@ -1,32 +1,49 @@
 package simulation;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SimulationEnvironment {
 	
+	
+	
 	public static void main(String[] args) {
 		int runs;
+		Simulator simulator = null;
+		ArrayList<Simulator> simulators = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("How many simulations do you want to do?:");
 		
 		while(true){
 			while(!(sc.hasNextInt())){
+				System.out.print("Write a number:");
 				sc.next();
 			}//while
 			
 			runs = sc.nextInt();
 			if(runs > 0){
-				System.out.print("Runs = " + runs);
 				break;
+			}//if
+			else{
+				System.out.print("The number of simulations has to be greater than 0:");
 			}
 		}//while
 		
-		System.out.print("Runs = " + runs);
+		for(int i = 0; i < runs; i++){
+			simulators.add(new Simulator("output/output_" + i + ".txt"));
+		}//for
 		
-		//Simulator simulator = new Simulator(4, 1);
-		//simulator.runSimulation();
-		
+		for(int i = 0; i < runs;){
+			if(simulators.get(i).isRunning() == false){//If the simulator is not running
+				if(simulators.get(i).getStarted() == false){//If the simulator didn't start
+					simulators.get(i).runSimulation(); //runs the simulation
+				}
+				else{
+					i++; //will pass to the next simulator
+				}//if
+			}//if
+		}//for
 		
 	}
 
